480c6ce377e417bf0a57b9c1c3800fb748eed011
Kunaal Shah <kupshah@iu.edu>
2020-11-18 09:29:01 -0500

    moved work from google colab to git repo

--- project/code/project_analyses.py
+++
@@ -1,9 +0,0 @@
-# import csv
-import pandas as pd
-# import numpy
-# import sklearn
-# from sklearn import preprocessing
-
-df = pd.read_csv('../data/tracks2020.csv', encoding='windows-1252')
-pd.set_option('display.max_columns', None)
-print(df)
